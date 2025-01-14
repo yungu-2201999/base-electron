12d59d0635d9b7a27b74a8323986be08a484c365
yungu-2201999 <rguo1022@gmail.com>
2025-01-14 23:34:31 +0800

    update: first app

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  // 用户不在MacOS上时，关闭所有窗口退出应用
+  if (process.platform !== 'darwin') app.quit()
+})
