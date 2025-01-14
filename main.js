776bb1875eb5b698979900862e5f233041738006
yungu-2201999 <rguo1022@gmail.com>
2025-01-15 01:03:27 +0800

    update: ipc

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
@@ -12,12 +17,13 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 
+  ipcMain.handle('ping', () => 'pong')
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
 app.on('window-all-closed', () => {
-  // 用户不在MacOS上时，关闭所有窗口退出应用
+  // 用户不在MacOS上时，关闭所有窗口时退出应用
   if (process.platform !== 'darwin') app.quit()
 })
